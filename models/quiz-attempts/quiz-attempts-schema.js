aeb0bc52527ce194cab278495aa640129ef70fa3
Alex Pho <pho.a@husky.neu.edu>
2021-04-16 00:43:58 -0400

    removed id so it can be auto-gen

--- models/quiz-attempts/quiz-attempts-schema.js
+++ models/quiz-attempts/quiz-attempts-schema.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const questionsSchema = require('../questions/questions-schema')
 const quizAttemptsSchema = mongoose.Schema(
     {
-        _id: String,
         score: Number,
         quiz: {type: String, ref: 'QuizzesModel'},
         answers: [questionsSchema]
